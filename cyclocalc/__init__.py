33dd69898cc5ad9241a9ef4168658dc3809df255
pewiok <przemyslaw.wlodarczyk@intmail.pl>
2025-03-08 15:30:17 +0200

    Update pyproject and dunders

---
+++ cyclocalc/__init__.py
@@ -0,0 +1,10 @@
+__author__ = "Przemysław Włodarczyk"
+__email__ = "przemyslaw.wlodarczyk@intmail.pl"
+__version__ = "0.1.0"
+__license__ = "GPL-2.0-only"
+__copyright__ = "Copyright (c) 2025 Przemysław Włodarczyk"
+__url__ = "https://github.com/pewiok/cyclocalc"
+__description__ = (
+    "A program that analyzes source code ASTs to measure cyclomatic complexity "
+    "and generate security metrics highlighting areas susceptible to vulnerabilities."
+)
